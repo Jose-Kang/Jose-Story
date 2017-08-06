51a0ec8064332f32bdada957ef5a313dd7c07c68
Jose-Kang <30773096+Jose-Kang@users.noreply.github.com>
2017-08-06 23:16:28 +0900

    Update message.js

--- message.js
+++ message.js
@@ -1,58 +1,24 @@
 # message.php
- 
-<?php
- 
-// 요청을 받아 저장
-$data = json_decode(file_get_contents('php://input'), true);
- 
-// 받은 요청에서 content 항목 설정
-$content = $data["content"];
- 
- 
- 
-// '시작하기' 버튼 처리
-if( $content == "시작하기" )
-{
-echo <<< EOD
-    {
+router.post('/message', (req, res) => {
+    const _obj = {
+        user_key: req.body.user_key,
+        type: req.body.type,
+        content: req.body.content
+    };
+    let massage = {
         "message": {
-            "text": "아직 개발중이라 대답을 잘 못해도 이해해줘^^;"
+            "text": '응답 메세지...'
+        },
+        "keyboard": {
+            "type": "buttons",
+            "buttons": [
+                "메뉴1",
+                "메뉴2",
+                "메뉴3"
+            ]
         }
-    }
-EOD;
-}
-// '도움말' 버튼 처리
-else if( $content == "도움말")
-{
-echo <<< EOD
-    {
-        "message": {
-            "text": "이제 곧 정식 버전이 출시될거야. 조금만 기다려~~~"
-        }
-    }    
-EOD;
-}
-// '안녕'이란 단어가 포함되었을때 처리
-else if( strpos($content, "안녕") !== false )
-{
-echo <<< EOD
-    {
-        "message": {
-            "text": "안녕~~ 반가워 ㅎㅎ"
-        }
-    }    
-EOD;
-}
-// 그밖의 문장일때 
-else
-{
-echo <<< EOD
-    {
-        "message": {
-            "text": "나랑 놀자 ㅋㅋㅋ"
-        }
-    }    
-EOD;
-}
- 
-?>
+    };
+    res.set({
+        'content-type': 'application/json'
+    }).send(JSON.stringify(massage));
+});
